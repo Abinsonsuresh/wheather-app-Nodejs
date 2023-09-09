1c4430e750e7164fe903e6d7adf3760dc1e00ac8
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-09-10 00:44:55 +0530

    Data has been sent to the front end

--- index.js
+++ index.js
@@ -4,18 +4,31 @@ var requests = require('requests');
 
 const homeFile = fs.readFileSync("index.html", "utf-8")
 
+const replaceVal = (tempvalue, originalvalue)=>{
+let temprature = tempvalue.replace("{%temp%}", originalvalue.main.temp)
+return temprature
+}
+
 const server = http.createServer((req, res) => {
     if (req.url == "/") {
         requests("http://api.openweathermap.org/data/2.5/weather?q=kochi,in&APPID=14f5fc262f3672d944f809a6e2e6c833")
             .on('data',  (chunk) => {
                 const objdata = JSON.parse(chunk)
                 const arraydata = [objdata]
-                console.log(arraydata[0].main.temp)
+                // console.log(arraydata[0].main.temp)
+                const realtimedata = arraydata.map((value)=> 
+                (
+
+                    replaceVal(homeFile, value))
+                ).join("")
+                // console.log(realtimedata)
+                res.write(realtimedata)
             })
             .on('end',  (err) => {
                 if (err) return console.log('connection closed due to errors', err);
 
-                console.log('end');
+                // console.log('end');
+                res.end()
             });
     }
     else{
